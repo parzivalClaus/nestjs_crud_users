5c8d27e88da764b993121c43df1009100f952692
parzivalClaus <claudio@kaizenfotoprodutos.com.br>
2021-06-15 16:17:05 -0300

    crud finished

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://claudioGoncalves:MongoDbTest123456@backendtotalzero.1hyph.mongodb.net/test'),
+    UsersModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule { }
